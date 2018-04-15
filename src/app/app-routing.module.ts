ec9aafe7761c07af127f90ff90c665b602d4a751
Ksantacr <defonix0@gmail.com>
2018-04-15 11:37:40 -0500

    Agregando rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,21 @@ import { Routes, RouterModule } from '@angular/router';
 import {AppComponent} from './app.component';
 
 import { MaterialDemoComponent } from './material/material-demo/material-demo.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
     path: 'material-demo',
     component: MaterialDemoComponent
+  },
+  {
+    path: 'home',
+    component: HomeComponent
   }
 
 ];
