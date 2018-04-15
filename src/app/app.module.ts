57591f9fa78bcaa0acfbb209b88aa1b85d921c58
Ksantacr <defonix0@gmail.com>
2018-04-15 09:44:23 -0500

    Componente toolbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import { AppComponent } from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
 
+
+
 @NgModule({
   declarations: [
     AppComponent
