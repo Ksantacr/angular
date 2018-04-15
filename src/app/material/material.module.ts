a6257eb317f93b2ed8de0951490e162a38119be4
Ksantacr <defonix0@gmail.com>
2018-04-15 10:10:56 -0500

    Componentes Icon y Button agregados

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {MatToolbarModule} from '@angular/material/toolbar';
 
 import { MaterialDemoComponent } from './material-demo/material-demo.component';
-
+import {MatIconModule, MatButtonModule  , MatToolbarModule} from '@angular/material';
 
 
 @NgModule({
   imports: [
     CommonModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule
   ],
   exports: [
-    MatToolbarModule
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule
   ],
   declarations: [MaterialDemoComponent]
 })
