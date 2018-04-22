062ed1519fc846b1042b0eb7d04b6fc08a2ace9c
Ksantacr <defonix0@gmail.com>
2018-04-22 09:37:16 -0500

    Interface y Class

--- src/app/material/material-list/material-list.component.ts
+++ src/app/material/material-list/material-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Estudiante } from '../../shared/model/estudiante';
+import { ClassEstudiante } from '../../shared/model/class-estudiante';
 
 @Component({
   selector: 'app-material-list',
@@ -7,9 +9,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MaterialListComponent implements OnInit {
 
+  estudiante1: Estudiante;
+  estudiante2: Estudiante;
+  estudiante3: Estudiante;
+  estudiante4: ClassEstudiante;
+
   constructor() { }
 
   ngOnInit() {
+
+    console.log("ngOnInit function");
+
+    this.estudiante1 = {
+      id:1,
+      nombre: 'Kevin',
+      ciudad: 'Guayaquil'
+    }
+    this.estudiante2 = {
+      id:1,
+      nombre: 'Kevin',
+      ciudad: 'Guayaquil',
+      fotoUrl: '//lorempixel.com/200/200'
+    }
+    this.estudiante3 = {
+      id:1,
+      nombre: 'Kevin',
+      ciudad: 'Guayaquil'
+    }
+    this.estudiante4 = new ClassEstudiante(4, 'Kevin', 'Guayaquil')
+
+    setTimeout(() => {
+      this.estudiante4.nombre = "Dayi";
+      console.log(this.estudiante4.getEdad());
+    } , 3000);
+
   }
 
 }
---
+++ src/app/shared/model/class-estudiante.ts
@@ -0,0 +1,12 @@
+export class ClassEstudiante {
+
+    constructor(private id:number,
+    public nombre:string, private ciudad:string){
+
+
+    }
+    getEdad(): number{
+        return 0;
+    }
+
+}
