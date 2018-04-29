2cf02433bafdedeaaecfe35753b082bac5c4d9a3
Ksantacr <defonix0@gmail.com>
2018-04-29 09:53:59 -0500

    Spinner y captura de errores

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,13 +12,23 @@ export class HomeComponent implements OnInit {
 
   readonly DEFAUL_PICTURE:string = "https://yt3.ggpht.com/a-/AJLlDp1wqoHl_4lYD7G-If6NmIFF8-D1ONXKHabckA=s900-mo-c-c0xffffffff-rj-k-no";
   listaPersonajes: Array<Personaje>;
+  error:string;
 
   constructor(private listaApiService: ListaApiService) {
   }
 
   ngOnInit() {
     this.listaApiService.getApiPersonajes().subscribe((data:Array<Personaje>) => {
+      
+      //  setTimeout(() => {
+      // }, 3000);
       this.listaPersonajes = data;
+      
+    }, (e)=>{
+      // setTimeout(() => {
+      // }, 3000);
+      this.error = "Se ha producido un problema al cargar la información."; 
+      
     });
 
     
